055958ae6df581951e139709ff00dfe4c8bc5636
nagasri-30 <nagasri726@gmail.com>
2026-09-07 15:09:52 +0530

    Create _multiple_multiple.py

---
+++ _multiple_multiple.py
@@ -0,0 +1,16 @@
+class Calculator:
+    def add(self, a, b):
+        print("Addition:", a + b)
+
+    def subtract(self, a, b):
+        print("Subtraction:", a - b)
+
+    def multiply(self, a, b):
+        print("Multiplication:", a * b)
+
+
+calc = Calculator()
+
+calc.add(10, 5)
+calc.subtract(10, 5)
+calc.multiply(10, 5)
