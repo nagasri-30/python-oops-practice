136cb6c3259f04542d2b7014ec0232bc88549141
nagasri-30 <nagasri726@gmail.com>
2026-09-07 15:15:03 +0530

    Create _constructor.py

---
+++ _constructor.py
@@ -0,0 +1,12 @@
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def display(self):
+        print("Name:", self.name)
+        print("Salary:", self.salary)
+
+
+employee = Employee("Nagasri", 30000)
+employee.display()
