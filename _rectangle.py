8c29d75f72cdf083b86723c17404124d73fa0af1
nagasri-30 <nagasri726@gmail.com>
2026-09-07 15:16:31 +0530

    Create _rectangle.py

---
+++ _rectangle.py
@@ -0,0 +1,16 @@
+class Rectangle:
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+
+    def area(self):
+        return self.length * self.breadth
+
+    def perimeter(self):
+        return 2 * (self.length + self.breadth)
+
+
+rectangle = Rectangle(10, 5)
+
+print("Area:", rectangle.area())
+print("Perimeter:", rectangle.perimeter())
